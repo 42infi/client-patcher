0d00fb0af8236a59475c2052dc2d43503d1ef2dd
Daniel Reed <x@sys32.dev>
2021-12-27 17:57:45 -0500

    USE LESS Buffer.concat(), remove confusing `for(... file of file...)`

--- src/asar.js
+++ src/asar.js
@@ -8,13 +8,13 @@ class Asar {
 	constructor(){
 		this.header = { files: {} };
 	}
-	resolve(file, keep_slash = false){
-		return path.posix.resolve('/', file).replace(/\\/g, '/').slice(keep_slash ? 0 : 1);
+	resolve(input, keep_slash = false){
+		return path.posix.resolve('/', input).replace(/\\/g, '/').slice(keep_slash ? 0 : 1);
 	}
 	async insert_folder(folder, alias){
 		var iterate = async (folder, parent) => {
-			for(var file of await asar_fs.promises.readdir(folder)){
-				var location = path.join(folder, file),
+			for(let file of await asar_fs.promises.readdir(folder)){
+				let location = path.join(folder, file),
 					stat = await asar_fs.promises.stat(location);
 				
 				parent[file] = {};
@@ -28,51 +28,54 @@ class Asar {
 		
 		await iterate(folder, (this.header.files[alias] = { files: {} }).files);
 	}
-	async delete(file){
+	async delete(path){
+		path = this.resolve(path);
+		
 		var last_depth,
+			last_file,
 			depth = this.header;
 		
-		file = this.resolve(file);
+		for(let file of path.split('/'))if(!(depth = (last_depth = depth.files)[last_file = file]))throw new Error('File ' + file + ' not found');
 		
-		for(var file of file.split('/'))if(!(depth = (last_depth = depth.files)[file]))throw new Error('File ' + file + ' not found');
-		
-		return delete last_depth[file];
+		return delete last_depth[last_file];
 	}
-	async exists(file){
+	async exists(path){
+		path = this.resolve(path);
+		
 		var depth = this.header;
 		
-		file = this.resolve(file);
-		
-		for(var file of file.split('/'))if(!(depth = depth.files[file]))return false;
+		for(let file of path.split('/'))if(!(depth = depth.files[file]))return false;
 		
 		return true;
 	}
-	async create_folder(input_file){
+	async create_folder(path){
 		var depth = this.header;
 		
-		for(var file of this.resolve(input_file).split('/'))depth = depth.files[file] || (depth.files[file] = { files: {} });
+		for(let file of this.resolve(path).split('/'))depth = depth.files[file] || (depth.files[file] = { files: {} });
 	}
-	async write_file(input_file, data){
+	async write_file(path, data){
 		var depth = this.header;
 		
-		for(var file of this.resolve(input_file).split('/'))depth = depth.files[file] || (depth.files[file] = {});
+		for(let file of this.resolve(path).split('/'))depth = depth.files[file] || (depth.files[file] = {});
 		
 		depth.data = Buffer.from(data);
 	}
-	async read_folder(input_file){
+	async read_folder(path){
 		var depth = this.header;
 		
-		for(var file of this.resolve(input_file).split('/'))if(!file)continue;
-		else if(!(depth = depth.files[file]))throw new Error('File ' + input_file + ' not found');
+		for(let file of this.resolve(path).split('/'))if(!file)continue;
+		else if(!(depth = depth.files[file]))throw new Error(`File ${path} not found`);
 		
 		if(!depth.files)throw new TypeError('Attempting to read a file as a folder');
 		
 		return Object.keys(depth.files);
 	}
-	async read_file(input_file){
+	async read_file(path){
+		path = this.resolve(path);
+		
 		var depth = this.header;
 		
-		for(var file of this.resolve(input_file).split('/'))if(!(depth = depth.files[file]))throw new Error('File ' + input_file + ' not found');
+		for(let file of path.split('/'))if(!(depth = depth.files[file]))throw new Error(`File ${path} not found;`);
 		
 		if(depth.files)throw new TypeError('Attempting to read a folder as a file');
 		
@@ -86,8 +89,8 @@ class Asar {
 		
 		return output;
 	}
-	async open(file){
-		this.file = file;
+	async open(path){
+		this.file = path;
 		
 		var handle = await fs.promises.open(this.file, 'r'),
 			size_buf = Buffer.alloc(8);
@@ -116,12 +119,13 @@ class Asar {
 		}
 	}
 	async save(save){
-		var write = Buffer.alloc(0),
+		var write = [],
+			write_bl = 0, // bytelength
 			iterate = async (folder, tree) => {
 				var files = {};
 				
-				for(var name in folder.files){
-					var file = folder.files[name],
+				for(let name in folder.files){
+					let file = folder.files[name],
 						location = path.posix.join(...tree, name);
 					
 					if(file.files){ // folder
@@ -129,15 +133,19 @@ class Asar {
 					}else{
 						files[name] = {
 							size: file.size,
-							offset: write.byteLength + '',
+							offset: write_bl + '',
 						};
 						
 						if(file.data){
 							files[name].size = file.data.byteLength;
-							write = Buffer.concat([ write, file.data ]);
-							
+							write_bl += file.data.byteLength;
+							write.push(file.data);
 							delete file.data;
-						}else write = Buffer.concat([ write, await this.read_file(location) ]);
+						}else{
+							let data = await this.read_file(location);
+							write_bl += data.byteLength;
+							write.push(data);
+						}
 					}
 				}
 				
@@ -152,7 +160,7 @@ class Asar {
 		header_size_buf.writeUInt32LE(header.byteLength + 4, 8)
 		header_size_buf.writeUInt32LE(header.byteLength, 12)
 		
-		await fs.promises.writeFile(save || this.file, Buffer.concat([ header_size_buf, header, write ]));
+		await fs.promises.writeFile(save || this.file, Buffer.concat([ header_size_buf, header, ...write ]));
 		
 		this.header.size = header.byteLength;
 		this.header = iterated;
